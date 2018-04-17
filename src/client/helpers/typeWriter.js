cc222d1ab0e71c4eac6e7f19ebb1f936731aeed4
salam <salamdalloul93@gmail.com>
2018-04-17 14:27:37 +0300

    home page header style #9 #8

---
+++ src/client/helpers/typeWriter.js
@@ -0,0 +1,33 @@
+import $ from 'jquery';
+import anime from 'animejs';
+
+const typeWriter = () => {
+  $('.ml12').each(function () {
+    $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, '<span class=\'letter\'>$&</span>'));
+  });
+
+  const myTimeLine = anime.timeline({ loop: true });
+
+  myTimeLine.add({
+    targets: '.ml12 .letter',
+    translateX: [40,0],
+    translateZ: 0,
+    opacity: [0,1],
+    easing: 'easeOutExpo',
+    duration: 5000,
+    delay(el, i) {
+      return 500 + 30 * i;
+    }
+  }).add({
+    targets: '.ml12 .letter',
+    translateX: [0,-30],
+    opacity: [1,0],
+    easing: 'easeInExpo',
+    duration: 7000,
+    delay(el, i) {
+      return 100 + 30 * i;
+    }
+  });
+};
+
+export default typeWriter;
