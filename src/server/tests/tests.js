1c9b1a50dcc28e8656149e6d0392e5f4e8a883ff
sultanassi95 <mrsultanassi@gmail.com>
2018-05-12 16:46:19 +0300

    Add DB-Tests and Favicon RELATES: #28 #8

--- src/server/tests/tests.js
+++ src/server/tests/tests.js
@@ -1,10 +1,18 @@
 // Controllers Tests:
 
-// GET Tests.
+// GET Tests:
 require('./controllers/getOrgs')();
 require('./controllers/getCats')();
 require('./controllers/getSubCats')();
 require('./controllers/getOrgsSubCats')();
 
-// POST Tests.
+// POST Tests:
 require('./controllers/postOrg')();
+
+// Database Queries Tests:
+
+// GET Tests:
+
+// Insert Tests:
+require('./database/insertOrg')();
+require('./database/insertSubCats')();
